b22c53eb75a7a45a22d6855314402499e3e7b6e5
abd-maqableh <abd.maqableh@gmail.com>
2019-08-20 10:23:36 +0300

    Download React

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+
+
+    return (
+      <div className="App">
+        App
+      </div>
+    )
+  }
+}
+
+
+export default App;
